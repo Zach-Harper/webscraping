036e54c3ee77e26307d9ab1935bcf81b1e890bde
Zach Harper <zach_harper3@gmail.com>
2023-05-05 20:19:42 -0500

    changes

--- webscraping-project.py
+++ webscraping-project.py
@@ -5,6 +5,8 @@ import openpyxl as xl
 from openpyxl.styles import Font, PatternFill, colors
 from openpyxl import workbook
 from openpyxl.drawing.image import Image
+import keys
+from twilio.rest import Client
 
 ###before running this code it is important to first import Pillow###
 
@@ -13,6 +15,10 @@ ws = wb.active
 ws.title = 'Top Cryptocurrencies'
 sheet = wb['Top Cryptocurrencies']
 ws.showGridlines= False
+
+ws['B1'] = 'Currency'
+ws['C1'] = 'Current Value'
+ws['D1'] = 'Percent Change'
 
 ws.column_dimensions['A'].width = 30
 ws.column_dimensions['B'].width = 30
@@ -47,10 +53,6 @@ for row in cell_range:
     for cell in row:
         cell.alignment = xl.styles.Alignment(horizontal='center',vertical='bottom')
         cell.font = Font(name='Times New Roman', size=18,bold=True,italic=True)
-
-ws['B1'] = 'Coin'
-ws['C1'] = 'Value'
-ws['D1'] = 'Percent Change'
 
 coindesk = 'https://coindesk.com/data/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
@@ -100,3 +102,28 @@ for i, value in enumerate(percent5):
     cell.value = value
 
 wb.save('webscraping-project.xlsx')
+
+_price = [float(x.replace(',','').strip('$')) for x in price5]
+_percent = [float(x.strip('%'))*.01 for x in percent5]
+
+dollar_change = [_price[i] * _percent[i] for i in range(len(_price))]
+print(dollar_change)
+y=0
+for x in dollar_change:
+    if x > 5:
+        message = f'{name5[y]} price has gone up by more then $5!'
+
+        client = Client(keys.accountSID,keys.auth_token)
+        TWnumber = "+13204088142"
+        myphone = "+15129130434"
+        textmsg = client.messages.create(to=myphone,from_=TWnumber,body=message)
+        print(textmsg.status)
+    y=+1
+    if x <-5:
+        message = f'{name5[y]} price has gone down by more then $5.'
+
+        client = Client(keys.accountSID,keys.auth_token)
+        TWnumber = "+13204088142"
+        myphone = "+15129130434"
+        textmsg = client.messages.create(to=myphone,from_=TWnumber,body=message)
+        print(textmsg.status)
